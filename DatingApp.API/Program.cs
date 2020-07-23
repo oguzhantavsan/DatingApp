c7f16187f3f42d8374f959b82d5de5f86ce0d52c
Oguzhan Tavsan <oguzhantavsan@sabanciuniv.edu>
2020-07-23 12:58:34 +0300

    End of Section 3

--- DatingApp.API/Program.cs
+++ DatingApp.API/Program.cs
@@ -17,10 +17,14 @@ namespace DatingApp.API
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.ConfigureKestrel(serverOptions =>
+            {
+                // Set properties and call methods on options
+            })
+            .UseStartup<Startup>();
+        });
     }
 }
